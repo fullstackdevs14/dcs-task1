15596ac9aa4e7118e608293b6294929c5125c79f
fullstackdevs14 <fullstackdevs@outlook.com>
2020-06-07 18:45:41 -0700

    add UI blocks

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,15 +1,58 @@
-import React from "react";
-import CountdownHeader from "./CountdownHeader";
+import React, { useState } from "react";
+import cls from "classnames";
+
+import "./Countdown.scss";
 
 const Countdown = () => {
-  const handleStart = (d) => {
+  const [duration, setDuration] = useState("");
+  const [playing, setPlaying] = useState(false);
+  const [speed, setSpeed] = useState(1);
+
+  const handleInputChange = (ev) => {
+    setDuration(ev.target.value);
+  };
+
+  const handleStart = () => {
+    const d = parseInt(duration);
+    if (Number.isNaN(d)) {
+      alert("Please input correct duration");
+      return;
+    }
+
     console.log(d);
   };
 
   return (
-    <div>
-      <CountdownHeader onStart={handleStart} />
-      <div>Countdown</div>
+    <div className="countdown">
+      <div className="countdown-header">
+        <span>Countdown:</span>
+        <input
+          value={duration}
+          onChange={handleInputChange}
+          placeholder="(MIN)"
+        />
+        <button onClick={handleStart}>Start</button>
+      </div>
+      <div className="countdown-body">
+        <div className="status-text">More than halfway there!</div>
+        <div className="timer">
+          <span>15:31</span>
+          {playing ? (
+            <button>
+              <i className="material-icons">pause</i>
+            </button>
+          ) : (
+            <button>
+              <i className="material-icons">play_arrow</i>
+            </button>
+          )}
+        </div>
+      </div>
+      <div className="speed-controls">
+        <span className={cls({ selected: speed === 1 })}>1X</span>
+        <span className={cls({ selected: speed === 1.5 })}>1.5X</span>
+        <span className={cls({ selected: speed === 2 })}>2X</span>
+      </div>
     </div>
   );
 };
